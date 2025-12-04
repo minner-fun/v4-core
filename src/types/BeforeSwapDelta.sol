74e9697ef2d9cb5d67f7dcf8c4588da34afe9b18
qfm_ky <qifumin2017@163.com>
2025-12-04 16:01:22 +0800

    delta

--- src/types/BeforeSwapDelta.sol
+++ src/types/BeforeSwapDelta.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Return type of the beforeSwap hook.
+// Return type of the beforeSwap hook. 返回值类型为BeforeSwapDelta
 // Upper 128 bits is the delta in specified tokens. Lower 128 bits is delta in unspecified tokens (to match the afterSwap hook)
+// 上128位是指定代币的delta，下128位是未指定代币的delta（与afterSwap hook匹配）
 type BeforeSwapDelta is int256;
 
-// Creates a BeforeSwapDelta from specified and unspecified
+// Creates a BeforeSwapDelta from specified and unspecified 从指定和未指定创建一个BeforeSwapDelta
 function toBeforeSwapDelta(int128 deltaSpecified, int128 deltaUnspecified)
     pure
     returns (BeforeSwapDelta beforeSwapDelta)
@@ -15,21 +16,21 @@ function toBeforeSwapDelta(int128 deltaSpecified, int128 deltaUnspecified)
     }
 }
 
-/// @notice Library for getting the specified and unspecified deltas from the BeforeSwapDelta type
+/// @notice Library for getting the specified and unspecified deltas from the BeforeSwapDelta type 库，用于从BeforeSwapDelta类型中获取指定和未指定的delta
 library BeforeSwapDeltaLibrary {
-    /// @notice A BeforeSwapDelta of 0
+    /// @notice A BeforeSwapDelta of 0 一个BeforeSwapDelta的0值
     BeforeSwapDelta public constant ZERO_DELTA = BeforeSwapDelta.wrap(0);
 
-    /// extracts int128 from the upper 128 bits of the BeforeSwapDelta
-    /// returned by beforeSwap
+    /// extracts int128 from the upper 128 bits of the BeforeSwapDelta 从BeforeSwapDelta的上128位提取int128
+    /// returned by beforeSwap 由beforeSwap返回
     function getSpecifiedDelta(BeforeSwapDelta delta) internal pure returns (int128 deltaSpecified) {
         assembly ("memory-safe") {
             deltaSpecified := sar(128, delta)
         }
     }
 
-    /// extracts int128 from the lower 128 bits of the BeforeSwapDelta
-    /// returned by beforeSwap and afterSwap
+    /// extracts int128 from the lower 128 bits of the BeforeSwapDelta 从BeforeSwapDelta的下128位提取int128
+    /// returned by beforeSwap and afterSwap 由beforeSwap和afterSwap返回
     function getUnspecifiedDelta(BeforeSwapDelta delta) internal pure returns (int128 deltaUnspecified) {
         assembly ("memory-safe") {
             deltaUnspecified := signextend(15, delta)
